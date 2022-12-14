69dd7f874f730ce8ce3d31bc4586979b7176dab5
AlekseyVychuzhanin <vychuzhanin1998@mail.ru>
2022-12-14 12:45:33 +0700

    first commit

---
+++ PythonLessons/hello.py
@@ -0,0 +1,60 @@
+#value = None
+#print(type(value))
+
+#a = 123
+#b = 1.23
+#print(type(a))
+#print(type(b))
+
+#value = 12334
+#print(type(value))
+
+#s = 'hello world'
+
+#print(a, '-', b, '-', s)
+#print('{1} - {2} - {0}'.format(a, b, s))
+#print(f'{a} - {b} - {s}')
+
+#f = True
+#print(f)
+
+#list = ['1', '2', '3']
+#col = ['hello', 1,2,3,4.5, True]
+#print(list)
+#print(col)
+
+
+#print('input a')
+#a = int(input())
+#print('input b')
+#b = int(input())
+#print(a, '+', b, '=', a + b)
+#print('{} {}'.format(a, b))
+#print(f'{a} {b}')
+
+#a = 123
+#b = 321
+#c = a + b
+#print(c)
+
+#a = int(input('a = '))
+#b = int(input('b = '))
+#if a > b:
+#    print(a)
+#else:
+#    print(b)
+
+#original = 23
+#inverted = 0
+#while original != 0:
+#    inverted = inverted * 10 + (original % 10)
+#    original //= 10
+#print(inverted)
+
+#list = [1,2,3,4,10,5]
+#for i in list:
+#    print(i**2)
+
+#for i in range(1, 10, 2):
+#    print(i)
+
